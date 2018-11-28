e0f36ad5569e44775a06a0c9541d7156951999d0
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2018-11-28 04:05:46 +0200

    fix path

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import fs from 'fs';
 import url from 'url';
 import path from 'path';
 
-export default (requestUrl, pathToFile = '') => {
+export default (requestUrl, pathToFile = process.cwd()) => {
   const { hostname, pathname } = url.parse(requestUrl);
   const rawFileName = url.format({ hostname, pathname });
   const fileName = `${rawFileName.replace(/[^a-zA-Z0-9]/g, '-')}.html`;
-  const resultPath = path.join(__dirname, pathToFile, fileName);
+  const resultPath = path.join(pathToFile, fileName);
 
   return axios.get(requestUrl)
     .then(response => response.data)
