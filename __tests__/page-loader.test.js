3f797c3dc06af2c5741de79ca3f9a6a61926cd61
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2018-11-30 03:38:08 +0200

    add download files, change links, add tests

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -10,10 +10,10 @@ import loader from '../src';
 axios.defaults.adapter = httpAdapter;
 nock.disableNetConnect();
 
-test('Download html page', async () => {
+test('Download html page without links', async () => {
   const host = 'https://hexlet.io';
   const pathname = '/courses';
-  const body = 'html body';
+  const body = '<html><head></head><body></body></html>';
   const filePath = url.format({ host, pathname });
   const tempDirName = path.join(os.tmpdir(), 'hexlet-');
 
@@ -28,6 +28,37 @@ test('Download html page', async () => {
   expect(data).toBe(body);
 });
 
+test('Download html page with links, format links', async () => {
+  const host = 'https://hexlet.io';
+  const pathname = '/';
+  const pathnameImg = '/test/image.jpg';
+  const pathnameScript = '/test/script.js';
+  const pathnameLink = '/test/style.css';
+  const bodyForFiles = 'body';
+  const body = '<html><head></head><body><img src="test/image.jpg"><script src="test/script.js"></script><link href="test/style.css"></body></html>';
+  const resultBody = '<html><head></head><body><img src="hexlet-io_files/test-image.jpg"><script src="hexlet-io_files/test-script.js"></script><link href="hexlet-io_files/test-style.css"></body></html>';
+  const filePath = url.format({ host, pathname });
+  const tempDirName = path.join(os.tmpdir(), 'hexlet-');
+
+  nock(host)
+    .get(pathname)
+    .reply(200, body)
+    .get(pathnameImg)
+    .reply(200, bodyForFiles)
+    .get(pathnameScript)
+    .reply(200, bodyForFiles)
+    .get(pathnameLink)
+    .reply(200, bodyForFiles);
+
+  const tempDir = await fs.promises.mkdtemp(tempDirName);
+  await loader(filePath, tempDir);
+  const fileName = await fs.promises.readdir(tempDir);
+  const tempFilePath = path.join(tempDir, fileName[0]);
+  const data = await fs.promises.readFile(tempFilePath, 'utf8');
+
+  expect(data).toBe(resultBody);
+});
+
 test('Error', async () => {
   const host = 'https://hexlet.io';
   const pathname = '/fakepath';
@@ -38,6 +69,7 @@ test('Error', async () => {
 
   nock(host).get(pathname).reply(status, body);
   const tempDir = await fs.promises.mkdtemp(tempDirName);
+  const response = await loader(filePath, tempDir);
 
-  expect(loader(filePath, tempDir)).rejects.toThrow(Error);
+  expect(response.status).toBe(status);
 });
--- src/index.js
+++ src/index.js
@@ -4,33 +4,58 @@ import url from 'url';
 import cheerio from 'cheerio';
 import path from 'path';
 
-const mapping = {
+const tagMapping = {
   img: 'src',
   script: 'src',
   link: 'href',
 };
 
-// eslint-disable-next-line
-const getLinks = (html, baseUrl, tag) => {
+const getDirectLinks = (links, baseUrl) => {
+  const { protocol, host, pathname } = url.parse(baseUrl);
+  return links.map((link) => {
+    if (link[0] === '/') {
+      return url.format({ protocol, host, pathname: link });
+    }
+    return url.format({ protocol, host, pathname: `${pathname}${link}` });
+  });
+};
+
+const getFiles = (html) => {
   const $ = cheerio.load(html);
   const links = [];
-  $(tag).each((i, elem) => {
-    const link = $(elem).attr(mapping[tag]);
+  Object.keys(tagMapping).forEach(tag => $(tag).each((i, elem) => {
+    const link = $(elem).attr(tagMapping[tag]);
     if (link && !url.parse(link).hostname && link[1] !== '/') {
-      const { protocol, host, pathname } = url.parse(baseUrl);
-      const resultUrl = (link[0] === '/') ? url.format({ protocol, host, pathname: link }) : url.format({ protocol, host, pathname: `${pathname}/${link}` });
-      links.push(resultUrl);
+      links.push(link);
     }
-  });
+  }));
   return links;
 };
 
-const formatName = (baseUrl, extension = '') => {
+const formatName = (baseUrl, extension) => {
   const { hostname, pathname } = url.parse(baseUrl);
   const rawFileName = url.format({ hostname, pathname });
-  return `${rawFileName.replace(/[^a-zA-Z0-9]/g, '-')}${extension}`;
+  const rawFileNameWihtoutSlash = rawFileName[rawFileName.length - 1] === '/' ? rawFileName.slice(0, -1) : rawFileName;
+  if (extension) {
+    return `${rawFileNameWihtoutSlash.replace(/[^a-zA-Z0-9]/g, '-')}${extension}`;
+  }
+  const newExtension = path.extname(rawFileNameWihtoutSlash);
+  const rawFileNameWithoutExt = rawFileNameWihtoutSlash.split(newExtension)[0];
+  return `${rawFileNameWithoutExt.replace(/[^a-zA-Z0-9]/g, '-')}${newExtension}`;
 };
 
+const updateLocalLinks = (html, dirName) => {
+  const $ = cheerio.load(html);
+  Object.keys(tagMapping).forEach(tag => $(tag).map((i, elem) => {
+    const link = $(elem).attr(tagMapping[tag]);
+    if (link && !url.parse(link).hostname && link[1] !== '/') {
+      const newLink = `${dirName}/${formatName(link)}`;
+      return $(elem).attr(tagMapping[tag], newLink);
+    }
+    return '';
+  }));
+  return $.html();
+};
 
 export default (requestUrl, pathToFile) => {
   const fileName = formatName(requestUrl, '.html');
@@ -39,10 +64,26 @@ export default (requestUrl, pathToFile) => {
   const dirName = formatName(requestUrl, '_files');
   const pathForDir = path.join(pathToFile, dirName);
 
+  let filesLinks = [];
+
   return axios.get(requestUrl)
-    .then(response => fs.promises.writeFile(pathForHtml, response.data))
+    .then((response) => {
+      const files = getFiles(response.data);
+      filesLinks = getDirectLinks(files, requestUrl);
+      const htmlWithLocalLinks = updateLocalLinks(response.data, dirName);
+      return fs.promises.writeFile(pathForHtml, htmlWithLocalLinks);
+    })
     .then(() => fs.promises.mkdir(pathForDir))
+    .then(() => Promise.all(filesLinks.map(link => axios
+      .get(link, { responseType: 'arraybuffer' })
+      .then((response) => {
+        const { pathname } = url.parse(link);
+        const currentName = formatName(pathname.substring(1));
+        const pathForFile = `${pathForDir}/${currentName}`;
+        return fs.promises.writeFile(pathForFile, response.data);
+      }))))
     .catch((error) => {
-      throw new Error(error.message);
+      console.log(`${error.message}. Error with url: ${error.config.url}`);
+      return error.response;
     });
 };
